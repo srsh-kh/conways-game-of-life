6bc4ffcd6313534761b989a19eb63819ba00701b
srsh-kh <sur.khosh@gmail.com>
2024-09-25 22:48:06 +0330

    x

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main()
     Color GREY = {29, 29, 29, 255};
     const int WINDOW_WIDTH = 750;
     const int WINDOW_HEIGHT = 750;
+    const int CELL_SIZE = 25;
     int FPS = 12;
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
