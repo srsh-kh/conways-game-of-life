706eec027be9be11bb9105ca7b25a21def09e543
srsh-kh <sur.khosh@gmail.com>
2024-09-28 06:42:49 +0330

    x

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,9 @@ int main()
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
     SetTargetFPS(FPS);
     Simulation simulation(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE);
-    simulation.SetCellValue(3, 4, 1);
-    simulation.SetCellValue(3, 5, 1);
-    simulation.SetCellValue(4, 5, 1);
-    simulation.SetCellValue(2, 5, 1);
+    simulation.SetCellValue(5, 29, 1);
 
-    std::cout << simulation.CountLiveNeighbors(3, 4) <<std::endl;
+    //std::cout << simulation.CountLiveNeighbors(3, 4) <<std::endl;
 
     while (WindowShouldClose() == false)
     {
